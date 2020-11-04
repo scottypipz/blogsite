df3cb7dc9e96a7fd8a1554ea12daa01b16605d31
scottypipz <chefpipz@DESKTOP-MA770SE.localdomain>
2020-11-04 11:30:07 +0800

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">ABouuute</router-link>
+    <router-link to="/about">ABoussute</router-link>
   </div>
   <router-view/>
 </template>
