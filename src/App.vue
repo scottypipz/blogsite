ca98c744ebe768a11ada0387908cc65dc981065b
scottypipz <chefpipz@DESKTOP-MA770SE.localdomain>
2020-11-05 11:13:50 +0800

    Added footer and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,42 @@
 <template>
-  <navbar/>
-  <router-view/>
+  <Navbar/>
+  <main>
+    <router-view/>
+  </main>
+  <Footer/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Footer from '@/components/fragments/Footer.vue';
 import Navbar from '@/components/fragments/Navbar.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
+    Footer,
     Navbar,
   },
 });
 </script>
 
 <style lang="scss">
+@import url(https://fonts.googleapis.com/css?family=Nunito);
+@import './assets/scss/_variables.scss';
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Nunito', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+main {
+  background-color: $google-light-light;
+  flex: 1;
 }
 
 * {
@@ -32,5 +47,26 @@ export default defineComponent({
 
 a, u {
   text-decoration: none;
+}
+
+.no-select {
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+}
+
+.web-only {
+  display: none;
+}
+
+/** Web */
+@media screen and (min-width: 1000px) {
+  .web-only {
+    display: block;
+  }
 }
 </style>
--- src/components/fragments/Navbar.vue
+++ src/components/fragments/Navbar.vue
@@ -1,15 +1,25 @@
 <template>
-  <div id="nav">
-    <div class="container">
+  <nav>
+    <div class="container no-select">
       <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/writer">Writer</router-link>
+
+      <!-- ============ Guest Routes ============ -->
+      <router-link to="/writer">Be a Writer</router-link>
+
+      <!-- ============ Private Routes ============ -->
+      <router-link to="/profile" class="profile">
+        <div class="name web-only">
+          John Doe
+        </div>
+        <div class="profile-pic"></div>
+      </router-link>
     </div>
-  </div>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
-#nav {
+@import '../../assets/scss/_variables.scss';
+nav {
   position: relative;
   top: 0;
   left: 0;
@@ -17,19 +27,38 @@
 
   .container {
     width: 800px;
+    max-width: 100%;
     margin: 0 auto;
     padding: 1rem;
     display: -ms-flexbox;
     display: flex;
     -ms-flex-wrap: wrap;
     flex-wrap: wrap;
-    -ms-flex-align: flex-end;
-    align-items: flex-end;
+    -ms-flex-align: center;
+    align-items: center;
     -ms-flex-pack: justify;
     justify-content: flex-end;
   }
+
   a {
     padding: 0 1rem;
+  }
+
+  .profile {
+    display: flex;
+    -ms-flex-align: center;
+    align-items: center;
+
+    .name {
+      margin-right: 1rem;
+    }
+
+    .profile-pic {
+      width: 2rem;
+      height: 2rem;
+      border-radius: 50%;
+      background-color: $google-light-secondary;
+    }
   }
 }
 </style>
